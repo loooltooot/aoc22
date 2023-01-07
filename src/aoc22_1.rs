a5bea0905a05effd843f13de83b94910954e7643
Emir Zakrewski <frenkchambers@gmail.com>
2023-01-07 21:24:13 +0300

    ci: I actually have lied in previous commit

--- src/aoc22_1.rs
+++ src/aoc22_1.rs
@@ -6,7 +6,7 @@ pub fn solution() {
         .map(|inventory| inventory.split("\r\n").collect::<Vec<&str>>())
         .collect::<Vec<Vec<&str>>>();
 
-    let mut max: u32 = 0;
+    let mut max: Vec<u32> = vec![];
     elves_inventory_list.iter().for_each(|inventory| {
         let mut sum: u32 = 0;
         inventory.iter().for_each(|item| sum += item
@@ -14,8 +14,12 @@ pub fn solution() {
             .replace("\r", "")
             .parse::<u32>().unwrap());
 
-        if sum > max {max = sum;}
+        max.push(sum);
+        max.sort();
+        max.reverse();
+
+        if max.len() > 3 {max.pop();}
     });
 
-    println!("Max calories is: {max}");
+    println!("Max calories is: {}", max.iter().sum::<u32>());
 }
