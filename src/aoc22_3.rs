f3cc4f936155f7d3458f5359598d4a9ca1dd0df3
Emir Zakrewski <frenkchambers@gmail.com>
2023-01-09 18:59:21 +0300

    ci: third day is done

--- src/aoc22_3.rs
+++ src/aoc22_3.rs
@@ -4,17 +4,28 @@ pub fn solution() {
     let input = std::fs::read_to_string("stocks/3.txt")
         .expect("Incorrect path");
 
-    let mut errors_sum: usize = 0;
-    input.lines().for_each(|line| {
-        let (st_comp, nd_comp) = line.split_at(line.len() / 2);
-
-        for c in st_comp.chars() {
-            if nd_comp.contains(c) {
-                errors_sum += ALL_TYPES.find(c).unwrap() + 1;
-                break;
-            };
+    let mut badges_sum: usize = 0;
+    let mut group_rucksacks: Vec<&str> = Vec::with_capacity(3);
+    input.lines().zip((1..=3).into_iter().cycle()).for_each(|(line, counter)| {
+        match counter {
+            1 | 2 => group_rucksacks.push(line),
+            3 => {
+                let (st_elf, nd_elf, rd_elf) = (
+                    group_rucksacks.pop().unwrap(),
+                    group_rucksacks.pop().unwrap(),
+                    line
+                );
+        
+                for c in st_elf.chars() {
+                    if nd_elf.contains(c) && rd_elf.contains(c) {
+                        badges_sum += ALL_TYPES.find(c).unwrap() + 1;
+                        break;
+                    };
+                }
+            },
+            _ => panic!("Error in iter")
         }
     });
 
-    println!("Errors sum is: {errors_sum}");
+    println!("Badges sum is: {badges_sum}");
 }
